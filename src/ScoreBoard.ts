d1358811c36fe813361b370823c4272c4267283b
Ivan Bokhvalov <bvancha@gmail.com>
2025-10-06 23:29:02 +0200

    feat: add checks for duplicate match

--- src/ScoreBoard.ts
+++ src/ScoreBoard.ts
@@ -9,6 +9,12 @@ class ScoreBoard {
   private matches: Match[] = [];
 
   startMatch(homeTeam: string, awayTeam: string): void {
+    const exists = this.matches.some(
+      m => m.homeTeam === homeTeam && m.awayTeam === awayTeam
+    );
+    if (exists) {
+      throw new Error('Match already exists');
+    }
     this.matches.push({
       homeTeam,
       awayTeam,
